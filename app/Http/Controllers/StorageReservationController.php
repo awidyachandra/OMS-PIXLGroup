014bd132de06fc0d6acaaa2676a2a737d6a60c0d
awidyachandra <awidyachndr@gmail.com>
2026-06-02 21:29:53 +0700

    kembali seperti semula

--- app/Http/Controllers/StorageReservationController.php
+++ app/Http/Controllers/StorageReservationController.php
@@ -411,13 +411,8 @@ class StorageReservationController extends Controller
     =====================================
     */
 
-    $file = $request->file('return_photo');
-
-$filename = time() . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
-
-$file->move(public_path('proofs/return'), $filename);
-
-$returnPhotoPath = 'proofs/return/' . $filename;
+    $returnPhotoPath = $request->file('return_photo')
+        ->store('proofs/return', 'public');
 
     $order->update([
         'status' => 'return checking',
@@ -459,13 +454,9 @@ $returnPhotoPath = 'proofs/return/' . $filename;
     =====================================
     */
 
-    $file = $request->file('pickup_photo');
+    $pickupPhotoPath = $request->file('pickup_photo')
+        ->store('proofs/pickup', 'public');
 
-$filename = time() . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
-
-$file->move(public_path('proofs/pickup'), $filename);
-
-$pickupPhotoPath = 'proofs/pickup/' . $filename;
     foreach ($order->details as $detail) {
         Unit::where('kode_unit', $detail->kode_unit)
             ->update(['status' => 'rented']);
